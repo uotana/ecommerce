ae0470d198c50af4ff9e80fcd8a64758b6f5c768
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-04-03 17:49:50 -0300

    Refatora addProduct e deleteProduct passando a utilizar ProductNotFoundException e CartNotFoundException.

--- src/main/java/br/com/letscode/ecommerce/shop/cart/CartService.java
+++ src/main/java/br/com/letscode/ecommerce/shop/cart/CartService.java
@@ -1,10 +1,11 @@
 package br.com.letscode.ecommerce.shop.cart;
 
+import br.com.letscode.ecommerce.shop.exception.CartNotFoundException;
+import br.com.letscode.ecommerce.shop.exception.ProductNotFoundException;
 import br.com.letscode.ecommerce.shop.product.ProductEntity;
 import br.com.letscode.ecommerce.shop.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,32 +20,19 @@ public class CartService {
 
     public CartEntity addProduct(Long cartId, Long productId) {
         Optional<CartEntity> cartEntityOptional = cartRepository.findById(cartId);
+        CartEntity cartEntity = cartEntityOptional.orElseThrow(() -> new CartNotFoundException("Cart with id "+ cartId +" not found"));
         Optional<ProductEntity> productEntityOptional = productRepository.findById(productId);
-        if(cartEntityOptional.isPresent() && productEntityOptional.isPresent()){
-            log.info("Adding product " + productId + " to cart " + cartId);
-            CartEntity cartEntity = new CartEntity();
-            BeanUtils.copyProperties(cartEntityOptional.get(), cartEntity);
-
-            ProductEntity productEntity = new ProductEntity();
-            BeanUtils.copyProperties(productEntityOptional.get(), productEntity);
-            cartEntity.getProducts().add(productEntity);
-            return cartRepository.save(cartEntity);
-        }else{
-            throw new RuntimeException("Not found");
-        }
+        ProductEntity productEntity = productEntityOptional.orElseThrow(() -> new ProductNotFoundException("Product with id "+ productId +" not found"));
+        cartEntity.getProducts().add(productEntity);
+        return cartRepository.save(cartEntity);
     }
 
     public CartEntity deleteProduct(Long cartId, Long productId) {
         Optional<CartEntity> cartEntityOptional = cartRepository.findById(cartId);
+        CartEntity cartEntity = cartEntityOptional.orElseThrow(() -> new CartNotFoundException("Cart with id "+ cartId +" not found"));
         Optional<ProductEntity> productEntityOptional = productRepository.findById(productId);
-        if(cartEntityOptional.isPresent() && productEntityOptional.isPresent()){
-            log.info("Removing product " + productId + " from cart " + cartId);
-            CartEntity cartEntity = new CartEntity();
-            BeanUtils.copyProperties(cartEntityOptional.get(), cartEntity);
-            cartEntity.getProducts().remove(productEntityOptional.get());
-            return cartRepository.save(cartEntity);
-        }else{
-            throw new RuntimeException("Not found");
-        }
+        productEntityOptional.orElseThrow(() -> new ProductNotFoundException("Product with id "+ productId +" not found"));
+        cartEntity.getProducts().remove(productEntityOptional.get());
+        return cartRepository.save(cartEntity);
     }
 }
