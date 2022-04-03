5dabeac2a2cd917bea81e95a14a2ea9688e2faed
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-04-02 22:36:07 -0300

    Adiciona CartEntity.java e CartRepository.java

---
+++ src/main/java/br/com/letscode/ecommerce/shop/cart/CartEntity.java
@@ -0,0 +1,21 @@
+package br.com.letscode.ecommerce.shop.cart;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@NoArgsConstructor
+@Entity(name="CART")
+public class CartEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "CREATION_DATE")
+    private ZonedDateTime creationDate;
+
+    @Column(name = "UPDATE_DATE")
+    private ZonedDateTime updateDate;
+}
