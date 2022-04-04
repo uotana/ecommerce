a97a0778969c52c0bb74e8c47e329e55f29f3166
Vinícius Maciel <v.s.maciel@outlook.com>
2022-04-04 18:12:53 -0300

    Adiciona delete e findAll em ManufacturerService

--- src/main/java/br/com/letscode/ecommerce/shop/manufacturer/ManufacturerService.java
+++ src/main/java/br/com/letscode/ecommerce/shop/manufacturer/ManufacturerService.java
@@ -1,9 +1,13 @@
 package br.com.letscode.ecommerce.shop.manufacturer;
 
+import br.com.letscode.ecommerce.shop.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +28,16 @@ public class ManufacturerService {
             return repository.save(manufacturer);
         }
 
+    }
+
+    public void delete(Long manufacturerId) {
+        Optional<ManufacturerEntity> manufacturerEntityOptional = repository.findById(manufacturerId);
+        ManufacturerEntity manufacturer = manufacturerEntityOptional.orElseThrow(
+                () -> new ProductNotFoundException("Not found"));
+        repository.delete(manufacturer);
+    }
+
+    public Page findAll(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 }
