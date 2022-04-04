7a2f6a62e1e837456158ea8991d3e1ba6f532cc7
Vinícius Maciel <v.s.maciel@outlook.com>
2022-04-04 18:08:37 -0300

    Adiciona métodos para listar Manufacturer e deletar Manufacturer

--- src/main/java/br/com/letscode/ecommerce/shop/manufacturer/ManufacturerController.java
+++ src/main/java/br/com/letscode/ecommerce/shop/manufacturer/ManufacturerController.java
@@ -1,12 +1,13 @@
 package br.com.letscode.ecommerce.shop.manufacturer;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/manufacturer")
@@ -18,6 +19,18 @@ public class ManufacturerController {
     @PostMapping
     public ResponseEntity<ManufacturerEntity>save(@RequestBody ManufacturerRequest manufacturer){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(manufacturer));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ManufacturerEntity>>getAll(@PageableDefault(
+            page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable(value = "id") Long manufacturerId){
+       service.delete(manufacturerId);
+        return ResponseEntity.ok("Successfully deleted!");
     }
 
 }
