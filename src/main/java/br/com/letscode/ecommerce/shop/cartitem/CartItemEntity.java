8b6073ab85f787bf4a0078ef78839101d92f433e
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-04-07 22:06:16 -0300

    Adiciona CartItemEntity.

---
+++ src/main/java/br/com/letscode/ecommerce/shop/cartitem/CartItemEntity.java
@@ -0,0 +1,33 @@
+package br.com.letscode.ecommerce.shop.cartitem;
+
+import br.com.letscode.ecommerce.shop.cart.CartEntity;
+import br.com.letscode.ecommerce.shop.product.ProductEntity;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Data
+@Entity(name="CART_ITEM")
+public class CartItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+//    @OneToOne
+    @JoinColumn(name="CART_ID")
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name="PRODUCT_ID")
+    private ProductEntity product;
+
+    @Column(name="QUANTITY")
+    private Integer quantity = 0;
+
+    @Column(name = "CREATION_DATE")
+    private ZonedDateTime creationDate;
+
+    @Column(name = "UPDATE_DATE")
+    private ZonedDateTime updateDate;
+}
