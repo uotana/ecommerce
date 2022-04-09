84dcef5bb1459fbf31081ea0efadcc6b46a79ec6
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-04-09 18:30:09 -0300

    Implementa UserDetailsServiceImpl

---
+++ src/main/java/br/com/letscode/ecommerce/shop/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package br.com.letscode.ecommerce.shop.auth;
+
+import br.com.letscode.ecommerce.shop.user.UserEntity;
+import br.com.letscode.ecommerce.shop.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username "+ username + " not found."));
+
+        return new User(username, userEntity.getPassword(), new ArrayList<>());
+//        return repository.findByUsername(username)
+//                .orElseThrow(() -> new UsernameNotFoundException("Username "+ username + " not found."));
+    }
+}
